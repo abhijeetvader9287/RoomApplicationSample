611d144cff4af887cb7b054116f92733b05695ac
Abhijeet Vader <a.vader@innopix.solutions>
2018-08-02 12:38:17 +0530

    first commit

---
+++ app/src/main/java/com/irontec/roomexample/database/daos/CustomerDao.kt
@@ -0,0 +1,32 @@
+package com.irontec.roomexample.database.daos
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+import com.irontec.roomexample.database.entities.Customer
+
+/**
+ * Created by axier on 7/2/18.
+ */
+
+@Dao
+interface CustomerDao {
+
+    @get:Query("SELECT * FROM customer")
+    val all: List<Customer>
+
+    @Query("SELECT * FROM customer WHERE uid IN (:customersId)")
+    fun loadAllByIds(customersId: Array<Int>): List<Customer>
+
+    @Query("SELECT * FROM customer WHERE first_name LIKE :first AND " + "last_name LIKE :last LIMIT 1")
+    fun findByName(first: String, last: String): Customer
+
+    @Insert
+    fun insertAll(customers: List<Customer>)
+
+    @Delete
+    fun delete(client: Customer)
+
+}
---
+++ app/src/main/java/com/irontec/roomexample/database/daos/ProviderDao.kt
@@ -0,0 +1,32 @@
+package com.irontec.roomexample.database.daos
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+import com.irontec.roomexample.database.entities.Provider
+
+/**
+ * Created by axier on 7/2/18.
+ */
+
+@Dao
+interface ProviderDao {
+
+    @get:Query("SELECT * FROM provider")
+    val all: List<Provider>
+
+    @Query("SELECT * FROM provider WHERE uid IN (:providerIds)")
+    fun loadAllByIds(providerIds: IntArray): List<Provider>
+
+    @Query("SELECT * FROM provider WHERE name LIKE :name LIMIT 1")
+    fun findByName(name: String): Provider
+
+    @Insert
+    fun insertAll(providers: List<Provider>)
+
+    @Delete
+    fun delete(provider: Provider)
+
+}
